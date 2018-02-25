cb4848fa8158f27c4ab51c5d64071e26d035447b
Bradley Serlis <bradleyserlis@Bradleys-Air.hsd1.wa.comcast.net>
2018-02-25 14:53:36 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log("up and running!");
+
+var cardOne = "queen",
+    cardTwo = "queen",
+    cardThree = "king",
+    cardFour = "king";
+
+console.log("User flipped " + cardOne + " and " + cardTwo);
