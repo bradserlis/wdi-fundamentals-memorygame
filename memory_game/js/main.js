d6db87455215a35d59ddfb56ef94c232f1b0256b
Bradley Serlis <bradleyserlis@Bradleys-Air.hsd1.wa.comcast.net>
2018-02-26 22:05:34 -0800

    created game logic - finish unit 11

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,7 +5,7 @@ var cards = [
 },
   {rank: "queen",
   suit: "diamonds",
-  cardImage: "images/queen-of-diamnds.png"
+  cardImage: "images/queen-of-diamonds.png"
 },
   {rank: "king",
   suit: "hearts",
@@ -28,18 +28,33 @@ var checkForMatch = function(){
   };
 };
 
-var flipCard = function(cardId){
+var flipCard = function(){
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   console.log("User flipped " + cards[cardId].suit);
   console.log("User flipped " + cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage)
   if(cardsInPlay.length === 2){
     checkForMatch();
   };
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+  for (var i = 0; i < cards.length; i++){
+  var cardElement = document.createElement('img');
+  cardElement.setAttribute('src', 'images/back.png');
+  cardElement.setAttribute('data-id', i);
+  cardElement.addEventListener('click', flipCard);
+  document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+
+
+createBoard();
+
+// flipCard(0);
+// flipCard(2);
 
 
 
