94f718c28c05a6e5098ff8de6f05b23f33a0dcae
Bradley Serlis <bradleyserlis@Bradleys-Air.hsd1.wa.comcast.net>
2018-02-26 16:35:19 -0800

    created game logic - unit 9

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,28 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[3];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length === 2){
-  if(cardsInPlay[0] === cardsInPlay[1]){
-    alert("YOU HAVE FOUND A MATCH!");
-  } else {
-    alert("Sorry, try again");
-  }
+var checkForMatch = function(){
+  {
+    if(cardsInPlay[0] === cardsInPlay[1]){
+      alert("YOU HAVE FOUND A MATCH!");
+    } else {
+      alert("Sorry, try again");
+    }
+  };
 };
+
+var flipCard = function(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  if(cardsInPlay.length === 2){
+    checkForMatch();
+  };
+};
+
+flipCard(0);
+flipCard(2);
+
+
 
 // console.log("User flipped " + cardOne + " and " + cardTwo);
 
